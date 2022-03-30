54f85d5c28ea65062ddcc087dafbe0dae48ef1e7
Abdoulaye Badji <58076640+ABDOULAYE2711@users.noreply.github.com>
2022-03-30 11:15:01 +0100

    Update My_app.py

--- My_app.py
+++ My_app.py
@@ -10,45 +10,68 @@ st.title("INFORMATION GAPS STUDY")
 st.header('IGS Results Senegal')
 
 
+#----write Image--------
 image = Image.open('LIFFT.png')
 
 st.image(image)
 
+#----write Dataframe--------
 st.markdown("# The database")
 df = pd.read_excel("IGS.xlsx")
-st.write(df)
+
+
+#----FILTER--------
+st.sidebar.header("Please Filter Here :")
+organization = st.sidebar.multiselect(
+    "Select the Organization:",
+    options=df['Type_of_Organization'].unique(),
+    default=df['Type_of_Organization'].unique()
+)
+
+
+country = st.sidebar.multiselect(
+    "Select the Country:",
+    options=df['Country'].unique(),
+    default=df['Country'].unique()
+)
+
+df_select = df.query(
+    "Type_of_Organization == @organization & Country == @country"
+)
+
+st.dataframe(df_select)
 
 # General and occupation
 st.markdown("# General and occupation")
-df1=df.iloc[:,0:9]
+df1=df_select.iloc[:,0:10]
 
 
 for i in df1.columns:
-    fig = px.histogram(df1, x = i, color_discrete_sequence=['#EF553B'], template='plotly_white')
+    fig = px.histogram(df1, x = i, color_discrete_sequence=['#EF553B'])
     st.plotly_chart(fig)
 
 # IT Assets and Connectivity
 st.markdown("# IT Assets and Connectivity")
-df2=df.iloc[:,10:44]
+df2=df_select.iloc[:,11:45]
 
 for i in df2.columns:
-    fig1 = px.histogram(df2, x = i, color_discrete_sequence=['#EF553B'], template='plotly_white')
+    fig1 = px.histogram(df2, x = i, color_discrete_sequence=['#EF553B'])
     st.plotly_chart(fig1)
 
 
 # Available information
 st.markdown("# Available informations")
-df3=df.iloc[:,45:78]
+df3=df_select.iloc[:,46:79]
 
 for i in df3.columns:
-    fig2 = px.histogram(df3, x = i, color_discrete_sequence=['#EF553B'], template='plotly_white')
+    fig2 = px.histogram(df3, x = i, color_discrete_sequence=['#EF553B'])
     st.plotly_chart(fig2)
 
 
 # Gaps information
 st.markdown("# Gaps informations")
-df4=df.iloc[:,95:110]
+df4=df_select.iloc[:,80:95]
 
 for i in df4:
-    fig3 = px.histogram(df4, x = i, color_discrete_sequence=['#EF553B'], template='plotly_white')
+    fig3 = px.histogram(df4, x = i, color_discrete_sequence=['#EF553B'])
     st.plotly_chart(fig3)
